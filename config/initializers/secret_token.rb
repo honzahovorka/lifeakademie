aa7e3a70c8e3febd3b525fc6d0c89f0606a73f2e
Honza Hovorka <mail@honzahovorka.cz>
2013-04-17 09:01:29 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeakademieCz::Application.config.secret_token = '6e0173a903341e120fba93339ef2a34c2b95de35037ff1bdfdce0ecb399df64da4fb4c261b7298f6e26043c2bb37be69464dd9a326519f4e4c25967c8e856f28'
