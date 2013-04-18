5625bcd73017ad315b73dd3bb1f204ec0a617ee0
Honza Hovorka <mail@honzahovorka.cz>
2013-04-18 12:16:26 +0200

    updated layout, added some content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def main
   end
+
+  def courses
+    if params[:subpage] == 'profil-absolventa'
+      render 'pages/courses/profile'
+    elsif params[:subpage] == 'ucebni-plan'
+      render 'pages/courses/course-plan'
+    elsif params[:subpage] == 'metody-vyuky'
+      render 'pages/courses/methods'
+    elsif params[:subpage] == 'certifikat'
+      render 'pages/courses/certificate'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 LifeakademieCz::Application.routes.draw do
+  match '/kurzy(/:subpage)' => 'pages#courses', via: :get, as: 'courses'
+  match '/terminy(/:location)' => 'pages#dates', via: :get, as: 'dates'
+  match '/o-nas' => 'pages#about', via: :get, as: 'about'
+  match '/kontakt' => 'pages#contact', via: :get, as: 'contact'
+
   root to: 'pages#main'
 end
