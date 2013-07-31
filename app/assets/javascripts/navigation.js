229ca6aa6d4e7d40cea975d38114711bf134120b
Honza Hovorka <honza.hovorka@gmail.com>
2013-07-31 10:08:24 +0200

    responsive navigation fixed, second try

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -2,41 +2,45 @@
 /* Main Navigation
 /* ---------------------------------------------------- */
 
+var firstRun = true;
+
 $(document).ready(function() {
 
-  (function() {
+    if (firstRun) {
 
       var arrowimages = {
           down: 'downarrowclass',
           right: 'rightarrowclass'
       };
-      var $mainNav = $('#navigation').find('ul'),
-              optionsList = '<option value="" selected>Navigace</option>';
 
-      var $submenu = $mainNav.find("ul").parent();
-      $submenu.each(function(i) {
-          var $curobj = $(this);
-          this.istopheader = $curobj.parents("ul").length == 1 ? true : false;
-          $curobj.children("a").append('<span class="' + (this.istopheader ? arrowimages.down : arrowimages.right) + '"></span>');
+      var mainNav = $('#navigation').find('ul.menu'),
+          optionsList = '<option value="" selected>Navigace</option>';
+
+      var submenu = mainNav.find("ul").parent();
+
+      submenu.each(function(i) {
+          var curobj = $(this);
+          $(this).istopheader = curobj.parents("ul").length == 1 ? true : false;
+          curobj.children("a").append('<span class="' + ($(this).istopheader ? arrowimages.down : arrowimages.right) + '"></span>');
       });
 
-      $mainNav.on('mouseenter', 'li', function() {
-          var $this = $(this),
-                  $subMenu = $this.children('ul');
-          if ($subMenu.length)
-              $this.addClass('hover');
-          $subMenu.hide().stop(true, true).fadeIn(200);
+      mainNav.on('mouseenter', 'li', function() {
+          var _this = $(this),
+              subMenu = _this.children('ul');
+          if (subMenu.length)
+              _this.addClass('hover');
+          subMenu.hide().stop(true, true).fadeIn(200);
       }).on('mouseleave', 'li', function() {
           $(this).removeClass('hover').children('ul').stop(true, true).fadeOut(50);
       });
 
       // Navigation Responsive
 
-      $mainNav.find('li').each(function() {
-          var $this = $(this),
-                  $anchor = $this.children('a'),
-                  depth = $this.parents('ul').length - 1,
-                  dash = '';
+      mainNav.find('li').each(function() {
+          var _this = $(this),
+              anchor = _this.children('a'),
+              depth = _this.parents('ul').length - 1,
+              dash = '';
 
           if (depth) {
               while (depth > 0) {
@@ -45,13 +49,15 @@ $(document).ready(function() {
               }
           }
 
-          optionsList += '<option value="' + $anchor.attr('href') + '">' + dash + ' ' + $anchor.text() + '</option>';
+          optionsList += '<option value="' + anchor.attr('href') + '">' + dash + ' ' + anchor.text() + '</option>';
 
       }).end()
         .after('<select class="nav-responsive">' + optionsList + '</select>');
 
-        $('.nav-responsive').on('change', function() {
-            window.location = $(this).val();
-        });
-  })();
+      $('.nav-responsive').on('change', function() {
+          window.location = $(this).val();
+      });
+
+      firstRun = false;
+    }
 });
