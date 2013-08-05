f866743dc5bd52b96345b78c3f7dacaeba6318d2
Honza Hovorka <honza.hovorka@gmail.com>
2013-08-05 12:10:07 +0200

    minor updated

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -9,7 +9,7 @@ class OrderMailer < ActionMailer::Base
     mail(
          to: order.user.email,
          subject: "Storno objednávky č. #{@order.variable_symbol} na lifeakademie.cz",
-         bcc: ['janh@starlife.cz']
+         bcc: ['janh@starlife.cz', 'info@lifeakademie.cz']
         )
   end
 end
